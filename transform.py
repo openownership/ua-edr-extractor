249b53f4f87a30b886a95e69fd9dcaa9d85b9592
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2017-10-08 14:46:14 +0300

    Docstrings for transform module

--- transform.py
+++ transform.py
@@ -1,3 +1,40 @@
+"""
+Whole idea of that project is to parse pulically available XML feed of ukrainian
+registry of companies and extract the information about beneficiary ownership into
+machine readable format (JSONL by default).
+
+Unfortunatelly, the task is not as simple as XML to JSON conversion, and here is why:
+The general data format of each record from input feed is following:
+<RECORD>
+    <NAME></NAME>
+    <SHORT_NAME></SHORT_NAME>
+    <EDRPOU></EDRPOU>
+    <ADDRESS></ADDRESS>
+    <BOSS></BOSS>
+    <KVED></KVED>
+    <STAN></STAN>
+    <FOUNDERS>
+        <FOUNDER></FOUNDER>
+        <FOUNDER></FOUNDER>
+        <FOUNDER></FOUNDER>
+    </FOUNDERS>
+</RECORD>
+
+The information that we need is stored in founders array, where each founder record is a raw string
+That record might describe the founder of the company OR beneficial owner (or both!). More over,
+beneficial ownership record usually carries not only the name of the owner, but also country of registration
+and address (which we also need). In some cases, such record might mention more than one owner or refer to
+legal entity as an owner. Finally, some records just saying that there are no beneficial owners or that the founder
+is also a beneficial owner (without naming him/her directly)
+
+This is a module which conducts the work of all elements of the pipeline:
+    Loading (opens XML feed downloaded from NAIS and does CP1251 to UTF-8 conversion)
+    Pre-processing (Tokenizes founder records using tokenizer, suitable for ukrainian language)
+    Categorization (Picks founder records which has information about beneficial ownership)
+    Parsing (Extracts the structured information about name/country/address of beneficial owner)
+    Saving (Saves the result in JSONL format (more formats coming soon))
+"""
+
 import argparse
 import logging
 import json
@@ -12,19 +49,51 @@ logger = logging.getLogger("transformer")
 
 
 class Transformer(object):
+    """
+    Umbrella class to run the sequence of steps to convert input xml
+    file into generator of parsed dicts
+    """
+
     def __init__(self, input_file):
+        """
+        Initializes Transformer class and instantiates all the pipeline
+        step classes
+
+        :param input_file: input XML file, downloaded from NAIS
+        :type input_file: str
+        """
+
         self.reader = EDRReader(input_file)
         self.preprocessor = PreProcessor(tokenize_words)
         self.beneficiary_categorizer = HasBeneficiaryOwnershipRecord()
         self.parser = HeuristicBasedParser()
 
     def parse_beneficial_owners(self, founders):
+        """
+        Applies categorizer to list of ownership records found in
+        a record of a company
+
+        :param founders: array of tokenized records about beneficial ownership
+        :type founders: list of lists of tokens
+        :returns: Results of parsing: name/country/address of owner (if any)
+        :rtype: list of dicts
+        """
         return list(map(
             self.parser.parse_founders_record,
             founders
         ))
 
     def transform_company(self, company):
+        """
+        Applies pre-process, categorization and parsing steps to record from the
+        company registry
+
+        :param company: One record from the input file
+        :type company: dict
+        :returns: Results of processing
+        :rtype: Dict
+        """
+
         founders = []
 
         for founder in self.preprocessor.process_founders(company):
@@ -38,6 +107,13 @@ class Transformer(object):
         }
 
     def pump_it(self):
+        """
+        Iterates over the input file and processes all the records found
+
+        :returns: processed companies
+        :rtype: iterator
+        """
+
         for company in self.reader.iter_docs():
             yield self.transform_company(company)
 
